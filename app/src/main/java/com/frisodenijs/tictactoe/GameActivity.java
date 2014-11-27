7ecb9125c7d52e23d1e98b125bebbb27ef7e51a8
anglean2002 <anglean2002@hotmail.com>
2014-11-27 15:49:33 +0100

    Change Button to ImageButton

--- app/src/main/java/com/frisodenijs/tictactoe/GameActivity.java
+++ app/src/main/java/com/frisodenijs/tictactoe/GameActivity.java
@@ -1,13 +1,16 @@
 package com.frisodenijs.tictactoe;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ImageButton;
 
 import com.frisodenijs.tictactoe.Game.Game;
 import com.frisodenijs.tictactoe.Game.Player;
@@ -18,32 +21,44 @@ public class GameActivity extends ActionBarActivity {
     private Game game;
 
     private TextView playerInfo;
-    private Button[][] buttons;
+    //private Button[][] imageButton;
+    ImageButton[][] imageButton = new ImageButton[3][3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        buttons = new Button[3][3];
+        //imageButton = new Button[3][3];
         playerInfo = (TextView) findViewById(R.id.playerInfo);
 
         // TODO: think about this. would be nice to avoid 9 lines of repeated code.
         /*
         for(int i = 0; i < 3; i++)
             for(int j = 0; j < 3; j++)
-                buttons[i][j] = (Button) findViewById(R.id.b00);
+                imageButton[i][j] = (Button) findViewById(R.id.b00);
         */
 
-        buttons[0][0] = (Button) findViewById(R.id.b00);
-        buttons[0][1] = (Button) findViewById(R.id.b01);
-        buttons[0][2] = (Button) findViewById(R.id.b02);
-        buttons[1][0] = (Button) findViewById(R.id.b10);
-        buttons[1][1] = (Button) findViewById(R.id.b11);
-        buttons[1][2] = (Button) findViewById(R.id.b12);
-        buttons[2][0] = (Button) findViewById(R.id.b20);
-        buttons[2][1] = (Button) findViewById(R.id.b21);
-        buttons[2][2] = (Button) findViewById(R.id.b22);
+        imageButton[0][0] = (ImageButton)findViewById(R.id.b00);
+        imageButton[0][1] = (ImageButton)findViewById(R.id.b01);
+        imageButton[0][2] = (ImageButton)findViewById(R.id.b02);
+        imageButton[1][0] = (ImageButton)findViewById(R.id.b10);
+        imageButton[1][1] = (ImageButton)findViewById(R.id.b11);
+        imageButton[1][2] = (ImageButton)findViewById(R.id.b12);
+        imageButton[2][0] = (ImageButton)findViewById(R.id.b20);
+        imageButton[2][1] = (ImageButton)findViewById(R.id.b21);
+        imageButton[2][2] = (ImageButton)findViewById(R.id.b22);
+
+
+        /*imageButton[0][0] = (Button) findViewById(R.id.b00);
+        imageButton[0][1] = (Button) findViewById(R.id.b01);
+        imageButton[0][2] = (Button) findViewById(R.id.b02);
+        imageButton[1][0] = (Button) findViewById(R.id.b10);
+        imageButton[1][1] = (Button) findViewById(R.id.b11);
+        imageButton[1][2] = (Button) findViewById(R.id.b12);
+        imageButton[2][0] = (Button) findViewById(R.id.b20);
+        imageButton[2][1] = (Button) findViewById(R.id.b21);
+        imageButton[2][2] = (Button) findViewById(R.id.b22);*/
 
         // First time Activity is created, take data from the intent.
         if (savedInstanceState == null) {
@@ -79,16 +94,23 @@ public class GameActivity extends ActionBarActivity {
      */
 
     private void updateGUI() {
-        // TODO: get board status and update all buttons or imagebuttons in the activity
+        // TODO: get board status and update all imageButton or imageimageButton in the activity
 
         Player[][] board = game.getBoard();
 
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++) {
-                if (board[i][j] != null)
-                    buttons[i][j].setText(board[i][j].toString());
-                else
-                    buttons[i][j].setText("");
+                if (board[i][j] != null) {
+                    if(board[i][j].toString().equals("O"))
+                        imageButton[i][j].setImageResource(R.drawable.pressed);
+                    else
+                        imageButton[i][j].setImageResource(R.drawable.x);
+                    //imageButton[i][j].setText(board[i][j].toString());
+                }
+               else{
+                    imageButton[i][j].setImageResource(R.drawable.ic_launcher);
+                    //imageButton[i][j].setText("");
+                }
             }
 
         playerInfo.setText("Current Player: " + game.getCurrentPlayer().toString());
@@ -96,17 +118,17 @@ public class GameActivity extends ActionBarActivity {
     }
 
     /*
-     * onClick Buttons functionality
+     * onClick imageButton functionality
      */
 
     public void onClickField(View view) {
 
-        // TODO: do not update view directly. 1: Update board. 2: Re-draw the buttons
-        Button buttonPressed = (Button) findViewById(view.getId());
+        // TODO: do not update view directly. 1: Update board. 2: Re-draw the imageButton
+        ImageButton buttonPressed = (ImageButton) findViewById(view.getId());
 
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
-                if (buttonPressed.equals(buttons[i][j])) {
+                if (buttonPressed.equals(imageButton[i][j])) {
 
                     if (game.makeMove(new int[]{i, j})) {
                         // If move is valid
@@ -133,7 +155,7 @@ public class GameActivity extends ActionBarActivity {
     }
 
      /*
-     * onClick Buttons functionality
+     * onClick imageButton functionality
      */
 
     public void goToFinishGame() {
