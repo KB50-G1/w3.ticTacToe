925ab9ea530b20044f03125d02df498fc171de7d
Friso1990 <friso1990@gmail.com>
2014-11-27 12:09:15 +0100

    Made AI (still has some TODOs)

--- app/src/main/java/com/frisodenijs/tictactoe/Game/AIPlayer.java
+++ app/src/main/java/com/frisodenijs/tictactoe/Game/AIPlayer.java
@@ -10,15 +10,166 @@ public class AIPlayer extends Player {
     }
 
     @Override
-    public int[] makeAutoMove() {
+    public int[] makeAutoMove(Board board) {
 
         // TODO: implement AI really difficult to understand methods.
 
         // TODO: this method needs to be able to read the board. How can we do this? Observer pattern?
 
-        int posX = 0;
-        int posY = 0;
-
-        return new int[]{posX, posY};
+        if (winningMove(board).length == 2) {
+            return winningMove(board);
+        } else if (forkingMove(board).length == 2) {
+            return forkingMove(board);
+        } else if (blockForkingMove(board).length == 2) {
+            return blockForkingMove(board);
+        } else if (centerMove(board).length == 2) {
+            return centerMove(board);
+        } else if (opposingCornerMove(board).length == 2) {
+            return opposingCornerMove(board);
+        } else if (emptyCornerMove(board).length == 2) {
+            return emptyCornerMove(board);
+        } else if (emptySideMove(board).length == 2) {
+            return emptySideMove(board);
+        } else {
+            return null;
+        }
     }
+
+    private int[] winningMove(Board board){
+        if (checkHorizontalWinning(board).length == 2) {
+            return checkHorizontalWinning(board);
+        } else if (checkVerticalWinning(board).length == 2) {
+            return checkVerticalWinning(board);
+        } else if (checkDiagonalWinning(board).length == 2) {
+            return checkDiagonalWinning(board);
+        } else {
+            return null;
+        }
+    }
+
+    private int[] forkingMove(Board board) {
+        //TODO
+        return null;
+    }
+
+    private int[] blockForkingMove(Board board) {
+        //TODO
+        return null;
+    }
+
+    private int[] centerMove(Board board) {
+        if (board.getBoard()[1][1] != null) {
+            return new int[] {1,1};
+        } else {
+            return null;
+        }
+    }
+
+    private int[] opposingCornerMove(Board board) {
+        if ((!board.getBoard()[0][0].getMark().equals(getMark())) && board.getBoard()[2][2] != null) {
+            return new int[] {2,2};
+        } else if ((!board.getBoard()[0][2].getMark().equals(getMark())) && board.getBoard()[2][0] != null) {
+            return new int[] {2,0};
+        } else if ((!board.getBoard()[2][0].getMark().equals(getMark())) && board.getBoard()[0][2] != null) {
+            return new int[] {0,2};
+        }else if ((!board.getBoard()[2][2].getMark().equals(getMark())) && board.getBoard()[0][0] != null) {
+            return new int[] {0,0};
+        } else {
+            return null;
+        }
+    }
+
+    private int[] emptyCornerMove(Board board) {
+        if (board.getBoard()[0][0] != null) {
+            return new int[] {0,0};
+        } else if (board.getBoard()[0][2] != null) {
+            return new int[] {0,2};
+        } else if (board.getBoard()[2][0] != null) {
+            return new int[] {2,0};
+        } else if (board.getBoard()[2][2] != null) {
+            return new int[] {2,2};
+        } else {
+            return null;
+        }
+    }
+
+    private int[] emptySideMove(Board board) {
+        if (board.getBoard()[0][1] != null) {
+            return new int[] {0,2};
+        } else if (board.getBoard()[1][0] != null) {
+            return new int[] {1,0};
+        } else if (board.getBoard()[1][2] != null) {
+            return new int[] {1,2};
+        } else if (board.getBoard()[2][1] != null) {
+            return new int[] {2,1};
+        } else {
+            return null;
+        }
+    }
+
+    private int[] checkHorizontalWinning(Board board) {
+        for (int y = 0; y < board.getBoard().length; y++) {
+            int thisCounter = 0;
+            int otherCounter = 0;
+            int[] position = null;
+            for (int x = 0; x < board.getBoard().length; x++) {
+
+                if (board.getBoard()[y][x] != null) {
+
+                    if (board.getBoard()[y][x].getMark().equals(getMark())) {
+                        thisCounter++;
+                        if (thisCounter == 2 & otherCounter == 0) {
+                            return position;
+                        }
+                    } else if (!board.getBoard()[y][x].getMark().equals(getMark())) {
+                        otherCounter++;
+                        if (otherCounter == 2 & thisCounter == 0) {
+                            return position;
+                        }
+                    }
+                } else {
+                    position = new int[]{y, x};
+                }
+            }
+        }
+        return null;
+    }
+
+    private int[] checkVerticalWinning(Board board) {
+        for (int y = 0; y < board.getBoard().length; y++) {
+            int thisCounter = 0;
+            int otherCounter = 0;
+            int[] position = null;
+            for (int x = 0; x < board.getBoard().length; x++) {
+
+                if (board.getBoard()[x][y] != null) {
+
+                    if (board.getBoard()[x][y].getMark().equals(getMark())) {
+                        thisCounter++;
+                        if (thisCounter == 2 & otherCounter == 0) {
+                            return position;
+                        }
+                    } else if (!board.getBoard()[x][y].getMark().equals(getMark())) {
+                        otherCounter--;
+                        if (otherCounter == 2 & thisCounter == 0) {
+                            return position;
+                        }
+                    }
+                } else {
+                    position = new int[]{x, y};
+                }
+            }
+        }
+        return null;
+    }
+
+    //TODO
+    private int[] checkDiagonalWinning(Board board) {
+        int thisCounter = 0;
+        int otherCounter = 0;
+        int[] position = null;
+
+        return null;
+    }
+
 }
