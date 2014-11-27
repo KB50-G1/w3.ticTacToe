cd74a7d8309fba504d90fb1b71a0f5bcbc442ced
Alfonso Pidal Ligués <alfonsopidal@gmail.com>
2014-11-27 19:53:21 +0100

    More changes! Please test and give feedback

--- app/src/main/java/com/frisodenijs/tictactoe/DialogEndGame.java
+++ app/src/main/java/com/frisodenijs/tictactoe/DialogEndGame.java
@@ -11,6 +11,8 @@ import android.os.Bundle;
  */
 public class DialogEndGame extends DialogFragment {
 
+    // TODO: lots of methods (and the class we extend) need android 11 or above!
+
     static DialogEndGame newInstance(String title) {
         DialogEndGame fragment = new DialogEndGame();
         Bundle args = new Bundle();
@@ -21,17 +23,26 @@ public class DialogEndGame extends DialogFragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // TODO: this function avoid the app crashing when the activity is destroyed while the dialog is shown
+        // TODO: but it dissapears, we need it back!
+        setRetainInstance(true);
+    }
+
+    @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         String title = getArguments().getString("title");
         return new AlertDialog.Builder(getActivity()).setIcon(R.drawable.ic_launcher)
                 .setTitle(title)
-                .setPositiveButton("OK",
+                .setPositiveButton("Yes!",
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int whichButton) {
                                 ((GameActivity) getActivity()).endGameDialogYes();
                             }
                         })
-                .setNegativeButton("Cancel",
+                .setNegativeButton("No",
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int whichButton) {
                                 ((GameActivity) getActivity()).endGameDialogNo();
