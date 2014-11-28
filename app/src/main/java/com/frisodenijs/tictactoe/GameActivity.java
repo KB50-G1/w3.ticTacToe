f45a2e9b960dfe0998a77569ca73f274173dd2a1
anglean2002 <anglean2002@hotmail.com>
2014-11-28 14:04:34 +0100

    Change Button to ImageView

--- app/src/main/java/com/frisodenijs/tictactoe/GameActivity.java
+++ app/src/main/java/com/frisodenijs/tictactoe/GameActivity.java
@@ -1,17 +1,17 @@
 package com.frisodenijs.tictactoe;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ImageButton;
-
 import com.frisodenijs.tictactoe.Game.Game;
 import com.frisodenijs.tictactoe.Game.Player;
 
@@ -19,36 +19,45 @@ import com.frisodenijs.tictactoe.Game.Player;
 public class GameActivity extends ActionBarActivity {
 
     private Game game;
+    private AnimationDrawable animationDrawable;
 
     private TextView playerInfo;
     //private Button[][] imageButton;
-    ImageButton[][] imageButton = new ImageButton[3][3];
+    ImageView[][] imageView = new ImageView[3][3];
+    private ImageView[][] animationIV = new ImageView[3][3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        //imageButton = new Button[3][3];
         playerInfo = (TextView) findViewById(R.id.playerInfo);
 
         // TODO: think about this. would be nice to avoid 9 lines of repeated code.
+
         /*
         for(int i = 0; i < 3; i++)
             for(int j = 0; j < 3; j++)
                 imageButton[i][j] = (Button) findViewById(R.id.b00);
         */
-
-        imageButton[0][0] = (ImageButton)findViewById(R.id.b00);
-        imageButton[0][1] = (ImageButton)findViewById(R.id.b01);
-        imageButton[0][2] = (ImageButton)findViewById(R.id.b02);
-        imageButton[1][0] = (ImageButton)findViewById(R.id.b10);
-        imageButton[1][1] = (ImageButton)findViewById(R.id.b11);
-        imageButton[1][2] = (ImageButton)findViewById(R.id.b12);
-        imageButton[2][0] = (ImageButton)findViewById(R.id.b20);
-        imageButton[2][1] = (ImageButton)findViewById(R.id.b21);
-        imageButton[2][2] = (ImageButton)findViewById(R.id.b22);
-
+        imageView[0][0] = (ImageView)findViewById(R.id.b00);
+        imageView[0][1] = (ImageView)findViewById(R.id.b01);
+        imageView[0][2] = (ImageView)findViewById(R.id.b02);
+        imageView[1][0] = (ImageView)findViewById(R.id.b10);
+        imageView[1][1] = (ImageView)findViewById(R.id.b11);
+        imageView[1][2] = (ImageView)findViewById(R.id.b12);
+        imageView[2][0] = (ImageView)findViewById(R.id.b20);
+        imageView[2][1] = (ImageView)findViewById(R.id.b21);
+        imageView[2][2] = (ImageView)findViewById(R.id.b22);
+        animationIV[0][0] = (ImageView) findViewById(R.id.b00);
+        animationIV[0][1] = (ImageView) findViewById(R.id.b01);
+        animationIV[0][2] = (ImageView) findViewById(R.id.b02);
+        animationIV[1][0] = (ImageView) findViewById(R.id.b10);
+        animationIV[1][1] = (ImageView) findViewById(R.id.b11);
+        animationIV[1][2] = (ImageView) findViewById(R.id.b12);
+        animationIV[2][0] = (ImageView) findViewById(R.id.b20);
+        animationIV[2][1] = (ImageView) findViewById(R.id.b21);
+        animationIV[2][2] = (ImageView) findViewById(R.id.b22);
 
         /*imageButton[0][0] = (Button) findViewById(R.id.b00);
         imageButton[0][1] = (Button) findViewById(R.id.b01);
@@ -94,24 +103,27 @@ public class GameActivity extends ActionBarActivity {
      */
 
     private void updateGUI() {
-        // TODO: get board status and update all imageButton or imageimageButton in the activity
+        // TODO: get board status and update all Button or imagButton in the activity
 
-        Player[][] board = game.getBoard();
+        final Player[][] board = game.getBoard();
 
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (board[i][j] != null) {
                     if(board[i][j].toString().equals("O"))
-                        imageButton[i][j].setImageResource(R.drawable.pressed);
-                    else
-                        imageButton[i][j].setImageResource(R.drawable.x);
-                    //imageButton[i][j].setText(board[i][j].toString());
+                        animationIV[i][j].setImageResource(R.drawable.o);
+                    if(board[i][j].toString().equals("X"))
+                        animationIV[i][j].setImageResource(R.drawable.x);
+                        animationDrawable = (AnimationDrawable) animationIV[i][j].getDrawable();
+                        animationDrawable.start();
                 }
-               else{
-                    imageButton[i][j].setImageResource(R.drawable.ic_launcher);
-                    //imageButton[i][j].setText("");
+                else {
+                    animationIV[i][j].setImageResource(R.drawable.clean);
+                    animationDrawable = (AnimationDrawable) animationIV[i][j].getDrawable();
+                    animationDrawable.start();
                 }
             }
+        }
 
         playerInfo.setText("Current Player: " + game.getCurrentPlayer().toString());
 
@@ -124,11 +136,11 @@ public class GameActivity extends ActionBarActivity {
     public void onClickField(View view) {
 
         // TODO: do not update view directly. 1: Update board. 2: Re-draw the imageButton
-        ImageButton buttonPressed = (ImageButton) findViewById(view.getId());
+        ImageView buttonPressed = (ImageView) findViewById(view.getId());
 
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
-                if (buttonPressed.equals(imageButton[i][j])) {
+                if (buttonPressed.equals(imageView[i][j])) {
 
                     if (game.makeMove(new int[]{i, j})) {
                         // If move is valid
