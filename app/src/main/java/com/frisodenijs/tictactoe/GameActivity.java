a2a213f72177db544a740e52b0ce1275a43a9621
Alfonso Pidal Ligués <alfonsopidal@gmail.com>
2014-11-29 20:31:51 +0100

    Added colors to players.
    * Optional feature: possibility to change colours from settings

--- app/src/main/java/com/frisodenijs/tictactoe/GameActivity.java
+++ app/src/main/java/com/frisodenijs/tictactoe/GameActivity.java
@@ -101,7 +101,12 @@ public class GameActivity extends ActionBarActivity {
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++) {
                 if (board[i][j] != null)
+                {
+                    // Place the player text (X or O) in the button.
                     buttons[i][j].setText(board[i][j].toString());
+                    // Change the color of the text.
+                    buttons[i][j].setTextColor(board[i][j].getColor());
+                }
                 else
                     buttons[i][j].setText("");
             }
@@ -116,6 +121,8 @@ public class GameActivity extends ActionBarActivity {
             }
 
             playersNames[i].setText(game.getPlayer(i).toString() + "\n" + game.getPlayer(i).getName());
+            playersNames[i].setTextColor(game.getPlayer(i).getColor());
+
             playersScores[i].setText(Integer.toString(game.getPlayer(i).getWinsCount()));
         }
 
